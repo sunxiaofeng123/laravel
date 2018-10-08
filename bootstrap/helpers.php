1e2ee7de45992b9e97520f580034f0247fcf0129
sunxiaofeng123 <18703137630@163.com>
2018-10-08 17:53:02 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,5 +4,13 @@
  * User: sunxiaofeng
  * Date: 2018/10/7
  * Time: 15:58
+ * composer.json 辅助函数文件自动加载。在命令行使用 composer dumpautoload
  */
 
+/**
+ * 此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class = "footer">
+    <div class = "container">
+        <p class = "pull-left">
+            由<a href = "http://blog.welcomegohome.com" traget = "_blank">FengFeng</a>设计和编码
+        </p>
+        <p class = "pull-right"><a href = "#">联系我们</a></p>
+    </div>
+</footer>
