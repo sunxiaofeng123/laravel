d21405c078354ac2aa6db28a72345bd52ea87401
sunxiaofeng123 <18703137630@163.com>
2018-11-07 23:15:13 +0800

    商品列表

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductsController extends Controller
+{
+    //展示列表
+    public function index(Request $request)
+    {
+        $products = Product::query()->where('on_sale', true)->paginate();
+
+        return view('products.index', ['products' => $products]);
+    }
+
+    //展示详情
+    public function show(Request $request, Product $product)
+    {
+        if (!$product->on_sale) {
+            throw new InvalidRequestException('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     //
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale'
+        'title', 'discription', 'image', 'on_sale'
         ,'rating', 'sold_count', 'review_count', 'price'
     ];
 
@@ -19,5 +20,14 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+            return $this->attributes['image'];
+        }
+
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
