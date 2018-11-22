3ac29cba296533100651c87c66ed50c3e97814c3
sunxiaofeng123 <18703137630@163.com>
2018-11-22 21:23:14 +0800

    删除商品

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AddCartRequest;
 use Illuminate\Http\Request;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 
 class CartController extends Controller
 {
@@ -34,5 +35,11 @@ class CartController extends Controller
         $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
 
         return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        return [];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::group(['middleware' => 'auth'], function() {
         //加入购物车
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::get('cart', 'CartController@index')->name('cart.index');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
     });
 });
