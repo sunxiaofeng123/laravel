a9fa0e293b9a1574ce17db0a7bddab9ba0a6d938
sunxiaofeng123 <18703137630@163.com>
2018-12-10 22:12:46 +0800

    整合业务逻辑

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -6,6 +6,7 @@ use App\Exceptions\InternalException;
 use App\Exceptions\InvalidRequestException;
 use App\Models\UserAddress;
 use App\Services\CartService;
+use App\Services\OrderService;
 use DemeterChain\C;
 use Illuminate\Http\Request;
 use App\Http\Requests\OrderRequest;
@@ -18,61 +19,12 @@ use App\Jobs\CloseOrder;
 class OrdersController extends Controller
 {
     //
-    public function store(OrderRequest $request, CartService $cartService)
+    public function store(OrderRequest $request, OrderService $orderService)
     {
         $user = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
 
-        //开始数据事务
-        $order = \DB::transaction(function()use($user, $request, $cartService){
-            $address = UserAddress::find($request->input('address_id'));
-
-            $address->update(['last_used_at' => Carbon::now()]);
-
-            //创建一个订单
-            $order = new Order([
-                'address' => [
-                    'address' => $address->full_address,
-                    'zip'     => $address->zip,
-                    'contact_name' => $address->contact_name,
-                    'contact_phone'=> $address->contact_phone,
-                ],
-                'remark'  => $request->input('remark'),
-                'total_amount' => 0,
-            ]);
-
-            $order->user()->associate($user);
-            $order->save();
-
-            $totalAmount = 0;
-            $items = $request->input('items');
-
-            foreach($items as $data) {
-                $sku = ProductSku::find($data['sku_id']);
-                $item = $order->items()->make([
-                    'amount' => $data['amount'],
-                    'price'  => $sku->price,
-                ]);
-
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku);
-                $item->save();
-                $totalAmount += $sku->price * $data['amount'];
-                if ($sku->decreaseStock($data['amount']) <= 0) {
-                    throw new InvalidRequestException('该商品库存不足');
-                }
-            }
-
-            $order->update(['total_amount' => $totalAmount]);
-
-            $skuIds = collect($request->input('items'))->pluck('sku_id')->all();
-            $cartService->remove($skuIds);
-
-            return $order;
-        });
-
-        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-
-        return $order;
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
 
     public function index(Request $request)
