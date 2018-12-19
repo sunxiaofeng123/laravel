5ba21ce5fe5df6472da8fd45bdb48161689f386e
sunxiaofeng123 <18703137630@163.com>
2018-12-19 22:55:25 +0800

    订单监听事件

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use Endroid\QrCode\QrCode;
 use App\Exceptions\InvalidRequestException;
+use App\Events\OrderPaid;
 
 class PaymentController extends Controller
 {
@@ -28,14 +29,41 @@ class PaymentController extends Controller
     //前端回调
     public function alipayReturn()
     {
-        $data = app('alipay')->verify();
+        try{
+            app('alipay')->verify();
+        }catch (\Exception $e){
+            return view('pages.error', ['msg' => '数据不正确']);
+        }
+
+        return view('pages,success', ['msg' => '付款成功']);
     }
 
     //服务器端回调
     public function alipayNotify()
     {
         $data = app('alipay')->verify();
-        \Log::debug('Alipay notify', $data->all());
+
+        if (!in_array($data->trade_status, ['TRADE_SUCCESS', 'TRADE_FINISHED'])) {
+            return app('alipay')->success();
+        }
+
+        $order = Order::where('no', $data->out_trade_no)->first();
+        if (!$order) {
+            return 'fail';
+        }
+
+        if ($order->paid_at) {
+            return app('alipay')->success();
+        }
+
+        $order->update([
+            'paid_at' => Carbon::now(),
+            'payment_method' => 'alipay',
+            'payment_no' => $data->trade_no,
+        ]);
+
+        $this->afterPaid($order);
+        return app('alipay')->success();
     }
 
     public function payByWechat(Order $order, Request $request)
@@ -77,5 +105,14 @@ class PaymentController extends Controller
             'payment_method' => 'wechat',
             'payment_no'     => $data->transaction_id,
         ]);
+
+        $this->afterPaid($order);
+
+        return app('wechat_pay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 }
